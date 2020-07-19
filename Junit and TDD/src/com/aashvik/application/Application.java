2872086ccd8a9c31b04d169c167901c5bf4e9edc
Aashvik <60035403+Aashvik@users.noreply.github.com>
2020-07-19 12:14:13 +0530

    Add files via upload

---
+++ Junit and TDD/src/com/aashvik/application/Application.java
@@ -0,0 +1,9 @@
+package com.aashvik.application;
+
+public class Application {
+	public static void main(String [] args )
+	{
+		System.out.println("Welcome to EEPAM website");
+		System.out.println("Here you can get info about our company and services");
+	}
+}
---
+++ Junit and TDD/src/com/aashvik/application/Application_TEst.java
@@ -0,0 +1,19 @@
+package com.aashvik.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Application_TEst {
+
+	/**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        assertTrue(true);
+    }
+
+}
